a777fdede0299f3933bc6c358cdb4591261a5f58
kivicamicmica <mohabs.mohamed@gmail.com>
2026-05-14 03:00:16 +0300

    Add files via upload

---
+++ New folder/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'coffee-master-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
